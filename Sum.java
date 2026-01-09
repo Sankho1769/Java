9f895bc2206f994a1857f33cbb3d21968cb1002b
Sankho1769 <banerjeeshubham00@gmail.com>
2026-01-09 18:08:43 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -4,7 +4,6 @@ public class Sum {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int a = sc.nextInt();
         int b = sc.nextInt();
 
@@ -13,3 +12,4 @@ public class Sum {
         sc.close();
     }
 }
+

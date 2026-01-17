f8770d640fc95b50726a923779a7b989b78edae1
Sankho1769 <banerjeeshubham00@gmail.com>
2026-01-17 10:28:49 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 5;
+
+        if (age >= 18) {
+            System.err.println("Adult");
+        } else {
+            System.err.println("Not Adult");
+        }
+    }
+}

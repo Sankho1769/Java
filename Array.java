f320379853085dea2de66a9564a63ebb35f4fea3
Sankho1769 <banerjeeshubham00@gmail.com>
2026-02-03 14:52:47 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = { 10, 50, 80, 90, 70 };
+
+        // System.out.println("The marks are: " + marks[0] + ", " + marks[1] + ", " + marks[2] + ", " + marks[3] + ", " + marks[4]);
+  
+        for(int i =0;i<marks.length;i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}

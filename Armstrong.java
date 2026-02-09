f5dbd19a5dee95c82b48d788bb5dc438e12f822f
Sankho1769 <banerjeeshubham00@gmail.com>
2026-02-09 07:47:23 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int temp = n, sum = 0;
+
+            while (n > 0) {
+                int d = n % 10;
+                sum = sum + d * d * d;
+                n = n / 10;
+            }
+
+            if (temp == sum)
+                System.out.println("Armstrong");
+            else
+                System.out.println("Not Armstrong");
+        }
+    }
+}

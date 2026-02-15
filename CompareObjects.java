ca1448ceeb7715749f29854017baf310850abe45
Sankho1769 <banerjeeshubham00@gmail.com>
2026-02-15 09:06:55 +0530

    Add files via upload

---
+++ CompareObjects.java
@@ -0,0 +1,11 @@
+public class CompareObjects {
+    public static void main(String[] args) {
+        String a = "Java";
+        String b = "Java";
+
+        if (a.equals(b))
+            System.out.println("Objects are equal");
+        else
+            System.out.println("Objects are not equal");
+    }
+}

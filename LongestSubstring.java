8b310a5eadeb9f64957c833f47390fa3c976f241
Sankho1769 <banerjeeshubham00@gmail.com>
2026-03-17 00:00:44 +0530

    Add files via upload

---
+++ LongestSubstring.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c) && map.get(c) >= left) {
+                left = map.get(c) + 1;
+            }
+
+            map.put(c, right);
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        int result = lengthOfLongestSubstring(s);
+
+        System.out.println("Longest substring length: " + result);
+    }
+}

506affddddeba540ab6cb21143e50be91e1b7e04
Sankho1769 <banerjeeshubham00@gmail.com>
2026-04-24 10:18:25 +0530

    Add files via upload

---
+++ Assignment_operator.java
@@ -0,0 +1,17 @@
+public class Assignment_operator {
+    public static void main(String[] args) {
+
+        // int x = 6;
+        // int y = 7;
+
+           double x = 8.9;
+           double y = 9.8; 
+
+           int a = 5;
+           int b = 10;
+
+        boolean result = x <= y ||  a > b;
+        System.out.println(result);
+
+    }       
+}

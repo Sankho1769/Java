c2134205fa7021fb5a0c40cbacc49498f82fbeca
Sankho1769 <banerjeeshubham00@gmail.com>
2026-04-29 00:01:57 +0530

    Add files via upload

---
+++ oporators.java
@@ -0,0 +1,16 @@
+public class oporators {
+    public static void main(String_Main[] args) {
+        int num1 = 7;
+        // int num2 = 5;
+
+        // int result = num1 + num2;
+        // System.out.println(result);
+
+        // num1 = num1 + 2;
+        // num1++
+        num1 *= 2;
+
+        System.out.println(num1);
+
+    }
+}

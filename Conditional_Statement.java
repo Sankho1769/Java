08c217b3b4b2ee9bbcf645ef6ddc83309f265b68
Sankho1769 <banerjeeshubham00@gmail.com>
2026-05-03 00:00:23 +0530

    Add files via upload

---
+++ Conditional_Statement.java
@@ -0,0 +1,37 @@
+public class Conditional_Statement {
+    public static void main (String_Main arg []) {
+        // int x = 18;
+
+        // if (x > 10  && x < 22) //accepted value 11 - 21
+        //     System.out.println("Hello");
+        // else
+        //     System.out.println("Bye");
+
+        //_______________________________________________________________________________________________________//
+
+        // int a = 8;
+        // int b = 7;
+
+        // if ( a > b ) {
+        //     System.out.println(a);
+        //     System.out.println("Thank You");
+        // }
+            
+        // else
+        //     System.out.println(b);
+
+      //_______________________________________________________________________________________________________//
+ 
+        int x  = 8;
+        int y = 7;
+        int z = 9;
+
+        if ( x < y &&  x > z) 
+            System.out.println(x);
+        else if ( y > z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+
+    }
+}

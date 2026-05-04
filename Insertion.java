511f24b8ce2ceaec85bf4476f5b609b622ab1eab
Sankho1769 <banerjeeshubham00@gmail.com>
2026-05-05 00:00:06 +0530

    Add files via upload

---
+++ Insertion.java
@@ -0,0 +1,28 @@
+public class Insertion {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 2};
+
+        insertionSort(arr);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void insertionSort(int arr[]) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+}

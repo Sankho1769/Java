d206af8c44c076d9e4434e638168d8780e31a504
Sankho1769 <banerjeeshubham00@gmail.com>
2026-05-04 07:52:36 +0530

    Add files via upload

---
+++ Type_Conversion.java
@@ -0,0 +1,25 @@
+public class Type_Conversion {
+    public static void main(String_Main[] arg) 
+    {
+        // byte b = 127;
+        // int a = b;
+
+        // int a = 12;
+        // byte k = (byte) a;
+        // System.out.println(k);
+
+        // System.out.println(a);
+
+        // float f = 5.6f;
+        // int t = (int) f;
+
+        // System.out.println(t);
+
+        byte a = 10;
+        byte b = 30;
+
+        int result = a * b;
+
+        System.out.println(result);
+    }
+}
